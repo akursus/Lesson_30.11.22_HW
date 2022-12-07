bb91c38257fb7a637691338e124b87e608a05ff7
akursus <ak_ursus@list.ru>
2022-12-07 21:12:48 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = arrayRandom(30);
+        //Task 1
+        System.out.println("Задание 1");
+        int sum = 0;
+        for (int number : array) {
+            sum += number;
+        }
+        System.out.println("Сумма трат за месяц составила " +sum +" рублей");
+
+
+
     }
+
+
+
+    public static int[] arrayRandom (int length) {
+        Random random = new Random();
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = random.nextInt(100_000) + 100_000;
+        }
+        return array;
+    }
+
 }
